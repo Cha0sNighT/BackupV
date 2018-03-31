edd934644d482a0a05d95daadde43071d319ffea
Cha0sNighT <chaosnight62@gmail.com>
2018-03-31 12:29:56 +0200

    Last

---
+++ [esxothers]/esx_map/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Map = {
+  {name="Tequil-La La",color=27, id=93, x=-565.906, y=276.093, z=100.176},
+  {name="Bahamas",color=27, id=93,x =-1388.409, y =-585.624, z =100.319},
+  {name="Yellow Jack",color=27, id=93,x =1992.69, y =3058.57, z =100.319},
+  {name="Garage Los Santos",color=3, id=290,x=56.31, y=-876.53, z=30.01},
+  {name="Kirammunation", color=49, id=432, x =-1303.33, y =-390.74, z=35.7},
+  {name="Garage Paleto Bay", color=3, id=290,x =60.0, y =6470.0, z =30.0},
+  {name="Garage Sandy Shores", color=3, id=290,x= 1723.0, y= 3713.0, z=33.0},
+  {name="Cinéma Downtown", color=25, id=135,x= 300.788, y= 200.752, z=104.385},
+  {name="Cinéma MorningWood", color=25, id=135,x= -1423.954, y= -213.62, z=46.5},
+  {name="Cinéma Vinewood", color=25, id=135,x= 302.907, y= 135.939, z=160.946}
+  --{name="SafeHouse",color=2, id=40,x=-107.8957366943, y=-8.305734634399, z=70.519645690918}
+}
---
+++ [esxothers]/esx_shops/locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+
+	['shop'] = 'Magasin',
+	['shops'] = 'Magasins',
+	['press_menu'] = 'appuyez sur ~INPUT_CONTEXT~ pour accéder au magasin.',
+	['bought'] = 'vous avez acheté ~b~1x ',
+	['not_enough'] = 'vous n\'avez ~r~pas assez~s~ d\'argent.',
+
+}
